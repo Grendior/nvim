c119a6567904ccfeadb6f5f6597516b7c19ed2c9
Szymon <szymon.fiedorowicz98@gmail.com>
2024-11-07 23:05:17 +0100

    some fixes

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,21 +18,21 @@ return {
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local mason_registry = require("mason-registry")
       local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
-          .. "/node_modules/@vue/language-server"
+      .. "/node_modules/@vue/language-server"
 
-    lspconfig.ts_ls.setup {
+      lspconfig.ts_ls.setup({
         capabilities = capabilities,
         init_options = {
-            plugins = {
-                {
-                    name = '@vue/typescript-plugin',
-                    location = vue_language_server_path,
-                    languages = { 'vue' },
-                },
+          plugins = {
+            {
+              name = '@vue/typescript-plugin',
+              location = vue_language_server_path,
+              languages = { 'vue' },
             },
+          },
         },
         filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-    }
+      })
       lspconfig.lua_ls.setup({ capabilities = capabilities })
       lspconfig.html.setup({ capabilities = capabilities })
       lspconfig.volar.setup({
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,12 +2,14 @@ return {
   "nvimtools/none-ls.nvim",
 
   config = function()
-    local none_ls = require("none-ls")
-    none_ls.setup({
+    local null_ls = require("null-ls")
+    null_ls.setup({
       sources = {
-        none_ls.builtins.formatting.stylua,
-        none_ls.builtins.formatting.prettier,
-        none_ls.builtins.completion.spell,
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier.with({
+            filetypes = { "vue", "javascript", "typescript", "css", "html" }
+        }),
+        null_ls.builtins.completion.spell,
         --require("none-ls.diagnostics.eslint"),
         --null_ls.builtins.diagnostics.eslint,
         --null_ls.builtins.code_actions.eslint,
